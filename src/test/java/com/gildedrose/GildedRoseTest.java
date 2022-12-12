bd1abd8a0012fc3df5b1ec7d221759c4b0ee3513
Jeong Dayeon <rina4674@naver.com>
2022-12-12 22:06:36 +0900

    Revert "add GildedRoseApprovalTest"
    This reverts commit 7648538b228eb54420db5a60e1b6daece8986fd9.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,9 +7,10 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[]{new Item("foo", 1, 1)};
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
 }
